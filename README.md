4e2456f5935786c3dcc47f808e802175907f5fbe
Luca <86166469+Calu0@users.noreply.github.com>
2024-03-15 18:13:16 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# PetitsPlats2.0
+
+P7 JS 2.0 Les petits plats
+
+npm run build:css
---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  "./**/*.{html,js}",
+  // "./scripts/pages/index.js",
+];
+
+export const theme = {
+  extend: {
+    colors: {
+      "yellow": "var(--yellow)",
+      "black": "var(--black)",
+    },
+    fontSize: {
+      xxxs: "0.625rem", // 10px
+      xxs: "0.75rem", // 12px
+      xs: "0.813rem", // 13px
+      sm: "0.875rem", // 14px
+      md: "0.938rem", // 15px
+      md2: "1rem", // 16px
+      lg: '1.125rem', // 18px
+      xl: '1.563rem', // 25px
+      '2xl': "1.875rem",  // 30px
+      '3xl': "2.5rem",  // 40px
+    },
+    backgroundImage: {
+      'banner': "url('/assets/images/banner.jpeg')",
+
+    },
+    fontFamily: {
+      "Manrope": ["'Manrope'", "sans-serif"],
+      "Anton": ["'Anton'", "sans-serif"],
+    },
+  },
+};
+
+export const plugins = [];
